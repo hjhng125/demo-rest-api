3a7051b5f106478466652e59c0d4139ecfe485f3
hjhng125 <hjhng125@nate.com>
2021-07-22 02:30:02 +0900

    EventControllerTest 응답 헤더 테스트 추가

--- src/test/java/me/hjhng125/restapi/event/EventControllerTest.java
+++ src/test/java/me/hjhng125/restapi/event/EventControllerTest.java
@@ -3,6 +3,7 @@ package me.hjhng125.restapi.event;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -13,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -55,7 +57,10 @@ public class EventControllerTest {
         )
             .andDo(print())
             .andExpect(status().isCreated())
-            .andExpect(jsonPath("id").exists());
+            .andExpect(jsonPath("id").exists())
+            .andExpect(header().exists(HttpHeaders.LOCATION))
+            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+        ;
 
     }
 }
