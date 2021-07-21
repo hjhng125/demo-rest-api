d5cb8ee5909e938d6565677eb82f716f17411981
hjhng125 <hjhng125@nate.com>
2021-07-22 02:18:05 +0900

    event controller 추가 event controller test code 작성

--- src/main/java/me/hjhng125/restapi/event/EventController.java
+++ src/main/java/me/hjhng125/restapi/event/EventController.java
@@ -1,11 +1,27 @@
 package me.hjhng125.restapi.event;
 
+import static org.springframework.hateoas.MediaTypes.HAL_JSON_VALUE;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/api/events", produces = HAL_JSON_VALUE)
 @RequiredArgsConstructor
 public class EventController {
 
 
+    @PostMapping
+    public ResponseEntity<Event> create(@RequestBody Event event) {
+        URI uri = linkTo(EventController.class).slash("{id}").toUri();
+        event.setId(1);
+        return ResponseEntity.created(uri)
+            .body(event);
+    }
 }
--- src/test/java/me/hjhng125/restapi/event/EventControllerTest.java
+++ src/test/java/me/hjhng125/restapi/event/EventControllerTest.java
@@ -2,8 +2,11 @@ package me.hjhng125.restapi.event;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -19,19 +22,32 @@ public class EventControllerTest {
     // 웹서버를 띄우지 않기 때문에 더 빠르다.
     // dispatcherServlet을 만들기에 타 단위테스트보다 빠르진 않다.
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     void createEvent_test() throws Exception {
-        //given
+        Event event = Event.builder()
+            .name("Spring")
+            .description("REST API")
+            .beginEnrollmentDateTime(LocalDateTime.of(2021, 7, 21, 0, 0, 0))
+            .closeEnrollmentDateTime(LocalDateTime.of(2021, 7, 28, 23, 59, 59))
+            .beginEventDateTime(LocalDateTime.of(2021, 8, 1, 0, 0, 0))
+            .closeEventDateTime(LocalDateTime.of(2021, 8, 1, 23, 59, 59))
+            .basePrice(100)
+            .maxPrice(200)
+            .limitOfEnrollment(100)
+            .location("강남역")
+            .build();
+
         mockMvc.perform(post("/api/events/")
             .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(event))
             .accept(MediaTypes.HAL_JSON)
         )
             .andDo(print())
-            .andExpect(status().isCreated());
-
-        //when
-
-        //then
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("id").exists());
 
     }
 }
